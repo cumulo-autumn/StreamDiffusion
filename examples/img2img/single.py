e8afcbfb8ca4d1b628016999720a348f65fb6687
ddPn08 <pyn.g.1125@gmail.com>
2023-12-04 17:02:06 +0900

    small fix

--- examples/img2img/single.py
+++ examples/img2img/single.py
@@ -11,11 +11,15 @@ from streamdiffusion.image_utils import pil2tensor, postprocess_image
 
 
 def main(input: str, output: str, prompt: str = "Girl with panda ears wearing a hood", scale: int = 1):
-    pipe: StableDiffusionPipeline = StableDiffusionPipeline.from_single_file("../../model.safetensors").to(
+    pipe: StableDiffusionPipeline = StableDiffusionPipeline.from_single_file("./model.safetensors").to(
         device=torch.device("cuda"),
         dtype=torch.float16,
     )
     pipe.enable_xformers_memory_efficient_attention()
+
+    input_image = PIL.Image.open(os.path.join(input))
+    width = int(input_image.width * scale)
+    height = int(input_image.height * scale)
 
     stream = StreamDiffusion(
         pipe, [32, 40, 45], torch_dtype=torch.float16, width=width, height=height, is_drawing=True
@@ -26,23 +30,6 @@ def main(input: str, output: str, prompt: str = "Girl with panda ears wearing a 
     stream.prepare(
         prompt,
         num_inference_steps=50,
-    )
-
-    input_image = PIL.Image.open(os.path.join(input))
-    width = int(input_image.width * scale)
-    height = int(input_image.height * scale)
-
-    stream = StreamDiffusion(
-        pipe,
-        [22, 32, 45],
-        torch_dtype=torch.float16,
-        width=width,
-        height=height,
-    )
-    stream.prepare(
-        "beach, sea, a palm tree, clouds",
-        num_inference_steps=50,
-        generator=torch.manual_seed(2),
     )
 
     input_image = input_image.resize((width, height))
