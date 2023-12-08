a2b648b17f1c1601e8eee7b20eb78676f234373d
ddPn08 <pyn.g.1125@gmail.com>
2023-12-08 22:12:20 +0900

    setup biome

--- demo/realtime-txt2img/view/vite.config.ts
+++ demo/realtime-txt2img/view/vite.config.ts
@@ -1,11 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        tsconfigPaths(),
-        react(),
-    ],
+	plugins: [tsconfigPaths(), react()],
 })
