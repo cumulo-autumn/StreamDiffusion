8b7f636da65b438eb97ea3edd3c97fe65497b2fb
kizamimi <miming28arteng035@gmail.com>
2023-12-10 22:44:03 +0900

    Add:コサイン類似度のべき乗変換

--- src/streamdiffusion/image_filter.py
+++ src/streamdiffusion/image_filter.py
@@ -1,18 +1,19 @@
 import torch
 
-
 class SimilarImageFilter:
-    def __init__(self, threshold: float = 0.95):
+    def __init__(self, threshold: float = 0.95, threshold_scale: float = 7):
+        self.threshold_scale = threshold_scale
         self.threshold = threshold
         self.prev_tensor = None
-
+        self.cos = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
+        
     def __call__(self, x: torch.Tensor):
         if self.prev_tensor is None:
             self.prev_tensor = x.detach().clone()
             return x
         else:
-            cos = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
-            output = cos(self.prev_tensor.reshape(-1), x.reshape(-1))
+            output = self.cos(self.prev_tensor.reshape(-1), x.reshape(-1))
+            output = ((output + 1) / 2) ** self.threshold_scale
             self.prev_tensor = x.detach().clone()
             if output.item() > self.threshold:
                 return None
