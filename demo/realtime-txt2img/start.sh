9ecd87b3906a90161b558458329152501511706f
Radamés Ajna <radamajna@gmail.com>
2023-12-18 21:48:30 -0800

    fix build script and make it mobile friendly

--- demo/realtime-txt2img/start.sh
+++ demo/realtime-txt2img/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd view && npm install && npm install -g serve && cd ..
-pip install -r requirements.txt
-cd view && npm run build && serve -s build -l 3000 &
+pip install -r requirements.txt 
+cd view && npm run build && cd ..
 cd server && python3 main.py
